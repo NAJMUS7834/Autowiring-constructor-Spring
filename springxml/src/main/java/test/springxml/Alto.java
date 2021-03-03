1010e2d21d825918efede4a5a7c4d59d17991f9a
Najmus Seemab <najmus.seemab@gmail.com>
2021-03-03 16:49:27 +0530

    Add files via upload

---
+++ springxml/src/main/java/test/springxml/Alto.java
@@ -0,0 +1,33 @@
+package test.springxml;
+
+public class Alto implements Car {
+	private int milage;
+	Engine engine;
+	
+	//constructor
+	public Alto(Engine engine) {
+		super();
+		this.engine = engine;
+	}
+
+
+
+	public void setEngine(Engine engine) {
+		this.engine = engine;
+	}
+
+
+
+	public void setMilage(int milage) {
+		this.milage = milage;
+	}
+
+
+
+	public void start() {
+		System.out.println("Alto Starting...with milage: "+milage+" with engine: ");
+		engine.carEngine();
+
+	}
+
+}
