23d62f93071b8b5da7b096d461de3aadfb8a4f5e
Najmus7834 <najmus.seemab@gmail.com>
2021-03-04 15:27:23 +0530

    Constructor Updated

--- springxml/src/main/java/test/springxml/Nano.java
+++ springxml/src/main/java/test/springxml/Nano.java
@@ -6,7 +6,7 @@ public class Nano implements Car {
 	private Engine engine; // instance Reference Variable
 	
 	
-	//constructor
+	//constructor calling constructor
 	public Nano(Engine engine) {
 		this.engine = engine;
 	}
